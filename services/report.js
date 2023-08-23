abd0ca9560bf2e1298e55633af93c0b57c397f51
mokhelek <mkatleho191@gmail.com>
2023-08-23 18:33:47 +0200

    report form query

--- services/report.js
+++ services/report.js
@@ -1,7 +1,10 @@
 export default function handleReport(db){ 
 
     async function addReport(){
-        // await db.none('INSERT INTO report ()', [hospital_id])
-
+       await db.none('INSERT INTO report(patient_id,doctor_id,type,date,description) VALUES ($1,$2,$3,$4,$5)', ['0105158745012','9818168745012','Prescription','12-25-2023','a detailed description from a doctor about the patient'] );
     }
+
+
+
+
 }
