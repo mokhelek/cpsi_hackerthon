9bfee73c1bf55599fdf113d8a0dd04233b98aa6b
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-08-23 12:22:51 +0200

    imported pgPromise and created an instance of the pgPromise

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import bodyParser from "body-parser";
+import pgPromise from "pg-promise";
 
 
 
 import flash from "express-flash";
 import session from "express-session";
 
-let app = express();
+// instances
+const app = express();
+const pgp = pgPromise();
+
+
 app.use(
     session({
         secret: "<add a secret string here>",
