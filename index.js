7d1b843b58d6a7b5bb36cdaa1728e45bad785536
mokhelek <mkatleho191@gmail.com>
2023-08-23 11:55:29 +0200

    server javascript file setup

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import bodyParser from "body-parser";
+
+
+
+import flash from "express-flash";
+import session from "express-session";
+
+let app = express();
+app.use(
+    session({
+        secret: "<add a secret string here>",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(flash());
+app.use(express.static("public"));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+app.get("/", (req, res)=>{
+    res.render('home')
+});
+
+
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+    console.log("App starting on port", PORT);
+});
+
+
