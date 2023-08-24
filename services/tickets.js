7ee6c403b7ae9acce7f1054dc24458a494e36cb1
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-08-24 09:15:16 +0200

    created a function to get the appointments

--- services/tickets.js
+++ services/tickets.js
@@ -5,9 +5,15 @@ export default function (db) {
 		const query = `SELECT * FROM report WHERE patient_id = '${patient_id}'`;
 		const tickets = db.manyOrNone(query);
 		return tickets;
-	}
+	};
+
+	async function getAppointment(doctor_id, type) {
+		const appointments = await db.manyOrNone(`Select * FROM report WHERE doctor_id = $1 AND type = $2`, [doctor_id, type]);
+		return appointments;
+	};
 
 	return {
-		getTickets
+		getTickets,
+		getAppointment
 	}
 }
