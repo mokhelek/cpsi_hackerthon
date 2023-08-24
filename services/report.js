a42b349a069b0a55bdd523dcf6d5b3fafbcb50a9
mokhelek <mkatleho191@gmail.com>
2023-08-24 10:58:26 +0200

    -

--- services/report.js
+++ services/report.js
@@ -1,7 +1,7 @@
 export default function handleReport(db){ 
 
     async function addReport(patientName,patientId, prescriptionType, description, dateTime ){
-       await db.none(`INSERT INTO report(patient_id,doctor_id,type,datetime,description,completed) VALUES ($1,$2,$3,$4,$5,$6)`, [patientId,'9818168745012',prescriptionType, dateTime,description,false] );
+       await db.none(`INSERT INTO report(patient_id,doctor_id,type,time,description,completed) VALUES ($1,$2,$3,$4,$5,$6)`, [patientId,'9818168745012',prescriptionType, dateTime,description,false] );
     }
 
     return {
