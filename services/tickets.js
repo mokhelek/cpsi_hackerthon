868b99a65d3c605da901c3c7de0a681ea6cbc49a
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-08-24 02:00:30 +0200

    added the type variable into the getTickets() as a argument

--- services/tickets.js
+++ services/tickets.js
@@ -1,5 +1,5 @@
 export default function (db) {
-	function getTickets(patient_id) {
+	function getTickets(patient_id, type) {
 		const query = `SELECT * FROM report WHERE patient_id = ${patient_id} AND type = ${type}`;
 		const tickets = db.manyOrNone(query);
 		return tickets;
